1a939b871330ed0787b7c4747ec2bf03e850f79a
shanbatla <shan.batla@gmail.com>
2015-12-10 13:25:07 -0800

    (test) Testing front end and back end configuration.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('ChallengeCtrl', function($scope) {
+.controller('ChallengeCtrl', function($scope, $http) {
   //set up mock data for challenge-feed template
   $scope.challengers = mockChallengers;
+
+  //Ajax request - test
+  //url for users that have signed in - http://bestrida.herokuapp.com/api/users
+  
+  $http({
+    method: 'GET',
+    url: 'http://bestrida.herokuapp.com/api/users'
+      }).then(function successCallback(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      console.log(response);
+      }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('error')
+  });
+
 
 })
 
@@ -42,5 +59,25 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ChallengeStatsCtrl', function($scope) {
-  console.log("clicked")
 });
+
+
+
+// Data format for Ajax request to user API
+// [
+  //{
+    // "_id":2711071,
+    // "firstname":"David",
+    // "lastname":"Lee",
+    // "token":"6040bda80f9dae9ca087fb5bb1a021c797e7636b",
+    // "photo":"https://dgalywyr863hv.cloudfront.net/pictures/athletes/2711071/3710377/1/large.jpg",
+    // "email":"david7lee@gmail.com",
+    // "__v":0
+  // },
+//]
+
+
+
+
+
+
