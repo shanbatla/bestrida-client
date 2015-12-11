4f507243b0736d43fef2d29025c2a86f9d28ed13
shanbatla <shan.batla@gmail.com>
2015-12-11 14:33:03 -0800

    (refactor) Incremental improvement.

---
+++ www/js/controllers/ActiveChallengesCtrl.js
@@ -0,0 +1,16 @@
+angular.module('activechallenges', [])
+
+.controller('ActiveChallengesCtrl', function($scope, ActiveChallenges) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.challengers = ActiveChallenges.all();
+  $scope.remove = function(challenger) {
+    ActiveChallenges.remove(challenger);
+  };
+});
--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,24 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('ChallengeCtrl', function($scope) {
-  //set up mock data for challenge-feed template
-  $scope.challengers = mockChallengers;
-})
+// .controller('ChallengeCtrl', function($scope) {
+//   //set up mock data for challenge-feed template
+//   $scope.challengers = mockChallengers;
+// })
 
-.controller('ActiveChallengesCtrl', function($scope, ActiveChallenges) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+// .controller('ActiveChallengesCtrl', function($scope, ActiveChallenges) {
+//   // With the new view caching in Ionic, Controllers are only called
+//   // when they are recreated or on app start, instead of every page change.
+//   // To listen for when this page is active (for example, to refresh data),
+//   // listen for the $ionicView.enter event:
+//   //
+//   //$scope.$on('$ionicView.enter', function(e) {
+//   //});
 
-  $scope.challengers = ActiveChallenges.all();
-  $scope.remove = function(challenger) {
-    ActiveChallenges.remove(challenger);
-  };
-})
+//   $scope.challengers = ActiveChallenges.all();
+//   $scope.remove = function(challenger) {
+//     ActiveChallenges.remove(challenger);
+//   };
+// })
 
 .controller('TailofTheTapeCtrl', function($scope, $stateParams, ActiveChallenges) {
   $scope.challenger = ActiveChallenges.get($stateParams.challengerID);
