a97bb28d457ba9eac6e2c11c49580e6d7629477d
shanbatla <shan.batla@gmail.com>
2015-12-10 22:42:47 -0800

    (refactor) Renamed relevant components.

--- www/js/app.js
+++ www/js/app.js
@@ -81,24 +81,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.active-challenges', {
+  // .state('tab.chats', {
+      url: '/active-challenges',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'active-challenges': {
+          templateUrl: 'templates/active-challenges.html',
+          controller: 'ActiveChallengesCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ActiveChallengeCtrl'
-        }
-      }
-    })
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:challengeId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'TailofTheTapeCtrl'
+    //     }
+    //   }
+    // })
 
   .state('tab.account', {
     url: '/account',
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,9 @@ angular.module('starter.controllers', [])
 .controller('ChallengeCtrl', function($scope) {
   //set up mock data for challenge-feed template
   $scope.challengers = mockChallengers;
-
 })
 
-.controller('ChatsCtrl', function($scope, ActiveChallenges) {
+.controller('ActiveChallengesCtrl', function($scope, ActiveChallenges) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -21,8 +20,9 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ActiveChallengeCtrl', function($scope, $stateParams, ActiveChallenges) {
+.controller('TailofTheTapeCtrl', function($scope, $stateParams, ActiveChallenges) {
   $scope.challenger = ActiveChallenges.get($stateParams.challengerID);
+  console.log("tale of the tape ctrl");
 })
 
 .controller('AccountCtrl', function($scope) {
