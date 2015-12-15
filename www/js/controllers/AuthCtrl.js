7dea666392af489b362de425562eed8d946db58d
shanbatla <shan.batla@gmail.com>
2015-12-14 19:26:37 -0800

    (feat) Login with ng cordova oauth seems to be functioning, still need to test functionality.

--- www/js/controllers/AuthCtrl.js
+++ www/js/controllers/AuthCtrl.js
@@ -1,12 +1,31 @@
-angular.module('auth', [])
+angular.module('auth', ['ngCordovaOauth'])
 
-.controller('AuthCtrl', function($scope) {
+.controller('AuthCtrl', function($scope, $http, $location, $cordovaOauth) {
+    
+    window.cordovaOauth = $cordovaOauth;
+    window.http = $http;
 
-    $scope.data = {};
+    // $scope.data = {};
  
     $scope.login = function() {
-      console.log("username: " + $scope.data.username + " and password: " + $scope.data.password);
-      window.open('http://bestrida.herokuapp.com/api/register', '_blank', 'location=yes');
+      // console.log("username: " + $scope.data.username + " and password: " + $scope.data.password);
+      
+      // Opens new in app window
+      // var ref = window.open('http://bestrida.herokuapp.com/api/register', '_blank', 'location=yes');
+      // ref.addEventListener('exit', function() {
+      //   console.log('In app browser closed');
+      // });
+
+      $cordovaOauth.strava(clientID, clientSecret, ['view_private']).then(function(result) {
+        console.log(result);
+      }, function(error) {
+        console.log(error);
+      });   
+
     }
 
 })
+
+
+//Strava Oauth
+// $cordovaOauth.strava(string clientId, string clientSecret, array appScope, object options);
