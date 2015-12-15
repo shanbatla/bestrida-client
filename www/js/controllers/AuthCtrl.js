66b42fd33a31fc85eead9315153b3f7d245f5f5a
shanbatla <shan.batla@gmail.com>
2015-12-15 14:58:37 -0800

    (feat) Auth apperas to be working, still need to test and verify.

--- www/js/controllers/AuthCtrl.js
+++ www/js/controllers/AuthCtrl.js
@@ -1,6 +1,6 @@
 angular.module('auth', ['ngCordovaOauth'])
 
-.controller('AuthCtrl', function($scope, $http, $location, $cordovaOauth) {
+.controller('AuthCtrl', function($scope, $http, $location, $state, $cordovaOauth) {
     
     window.cordovaOauth = $cordovaOauth;
     window.http = $http;
@@ -11,28 +11,25 @@ angular.module('auth', ['ngCordovaOauth'])
       // console.log("username: " + $scope.data.username + " and password: " + $scope.data.password);
       
       // Opens new in app window
-      // var ref = window.open('http://bestrida.herokuapp.com/api/register', '_blank', 'location=yes');
-      // ref.addEventListener('exit', function() {
-      //   console.log('In app browser closed');
-      // });
-
-      $cordovaOauth.strava(clientID, clientSecret, ['view_private']).then(function(response) {
-        //if response is truthy, redirect to challenge feed
-        if (response) {
-          //redirect to challenge feed
-          console.log("login success");
-        } else {
-          //keep user in login
-          console.log("login fail");
+      var ref = window.open('http://bestrida.herokuapp.com/api/register', '_blank', 'location=yes');
+      var ref = window.open('https://www.strava.com/oauth/authorize?' + 'client_id=' + clientID + '&response_type=code' + '&redirect_uri=http://localhost/callback ', '_blank', 'location=yes');
+      ref.addEventListener('loadstart', function(event) { 
+        if((event.url).startsWith("http://localhost/callback")) {
+          requestToken = (event.url).split("code=")[1];
+          ref.close();
+          $state.transitionTo('tab.challenge-feed');
         }
-      }, function(error) {
-        console.log(error);
-      });   
+      });
+
+      // $cordovaOauth.strava(clientID, clientSecret, ['view_private']).then(function(result) {
+      //   console.log("Response Object -> " + JSON.stringify(result));
+      //   // $location.path('/challenge-feed');
+      //   // $state.go('/challenge-feed');
+
+      // }, function(error) {
+      //   console.log(error);
+      // });   
 
     }
 
 })
-
-
-//Strava Oauth
-// $cordovaOauth.strava(string clientId, string clientSecret, array appScope, object options);
