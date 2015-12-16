b774c397d53e8b64c7d29f8a18ee88957d90e2b3
shanbatla <shan.batla@gmail.com>
2015-12-16 11:05:17 -0800

    (feat) Code clean up.

--- www/js/controllers/AuthCtrl.js
+++ www/js/controllers/AuthCtrl.js
@@ -2,26 +2,14 @@ angular.module('auth', ['ngCordovaOauth'])
 
 .controller('AuthCtrl', function($scope, $rootScope, $http, $location, $state, $cordovaOauth) {
     
-    window.cordovaOauth = $cordovaOauth;
-    window.http = $http;
+    // window.cordovaOauth = $cordovaOauth;
+    // window.http = $http;
 
     // $scope.data = {};
  
     $scope.login = function() {
       // console.log("username: " + $scope.data.username + " and password: " + $scope.data.password);
       
-      //DONT USE ANYMORE
-      // $cordovaOauth.strava(clientID, clientSecret, ['view_private']).then(function(result) {
-      //   console.log("Response Object -> " + JSON.stringify(result));
-      //   // $location.path('/challenge-feed');
-      //   // $state.go('/challenge-feed');
-
-      // }, function(error) {
-      //   console.log(error);
-      // });   s
-      
-      // Opens new in app window
-      // var ref = window.open('http://bestrida.herokuapp.com/api/register', '_blank', 'location=yes');
       var ref = window.open('https://www.strava.com/oauth/authorize?' + 'client_id=' + clientID + '&response_type=code' + '&redirect_uri=http://localhost/callback ', '_blank', 'location=yes');
       ref.addEventListener('loadstart', function(event) { 
         if((event.url).startsWith("http://localhost/callback")) {
@@ -43,8 +31,6 @@ angular.module('auth', ['ngCordovaOauth'])
           });  
         }
       });
-
-
     }
 
 })
