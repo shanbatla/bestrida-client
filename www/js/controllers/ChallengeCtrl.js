5b25e46618928e232a2173ec30b39d8a57c51ab5
Justin Zimmerman <zimmy130@gmail.com>
2015-12-19 17:51:46 -0500

    (feat) drag down to reload

--- www/js/controllers/ChallengeCtrl.js
+++ www/js/controllers/ChallengeCtrl.js
@@ -4,10 +4,20 @@ angular.module('challengers', [])
 
   $scope.user = AuthFct.user.athlete;
 
-  FeedFct.pendingChallenge($scope.user.id)
+  $scope.doRefresh = function() {
+    FeedFct.pendingChallenge($scope.user.id)
     .success(function(data) {
       $scope.challenges = data;
     })
+    .finally(function() {
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  FeedFct.pendingChallenge($scope.user.id)
+    .success(function(data) {
+      $scope.challenges = data;
+    });
     // TODO: error takes too long to display -> Option: replace with logic that will check the length of our data array and display a different line item if the length is 0
     // .error(function(error) {
     //   alert("Looks like you're fresh out of pending challenges. Get to work and challenge a friend!");
--- www/js/controllers/CompletedCtrl.js
+++ www/js/controllers/CompletedCtrl.js
@@ -4,6 +4,39 @@ angular.module('completed', [])
   
   $scope.user = AuthFct.user.athlete;
 
+  $scope.doRefresh = function() {
+    CompletedFct.getCompletedChallenge($scope.user.id)
+    .success(function(data) {
+      $scope.challenges = data;
+      $scope.challenges.forEach(function(challenge) {
+        if($scope.user.id !== challenge.challengeeId) {
+          challenge.opponent = challenge.challengeeName;
+        } else if($scope.user.id !== challenge.challengerId) {
+          challenge.opponent = challenge.challengerName;
+        } else {
+          challenge.opponent = "Opponent";
+        }
+        if(challenge.challengeeCompleted && challenge.challengerCompleted) {
+          if($scope.user.id === challenge.winnerId) {
+            challenge.completedStatus = "You won this challenge!";
+          } else if($scope.user.id === challenge.loserId) {
+            challenge.completedStatus = "You lost this challenge.";
+          }else {
+            challenge.completedStatus = "Challenge still calculating.";
+          }
+        } else {
+          challenge.completedStatus = "Waiting for other user to complete.";
+        }
+      });
+    })
+    .finally(function() {
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+    .error(function(error) {
+      alert(error);
+    });
+  };
+
   CompletedFct.getCompletedChallenge($scope.user.id)
     .success(function(data) {
       $scope.challenges = data;
