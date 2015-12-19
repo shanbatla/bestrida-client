8c86e3566831570c974af2ca2fc2c0884e17dd36
Justin Zimmerman <zimmy130@gmail.com>
2015-12-18 20:22:13 -0500

    (feat) pull active challenges

--- www/js/controllers/ActiveChallengesCtrl.js
+++ www/js/controllers/ActiveChallengesCtrl.js
@@ -1,16 +1,14 @@
 angular.module('activechallengesctrl', [])
 
-.controller('ActiveChallengesCtrl', function($scope, ActiveChallenges) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ActiveChallengesCtrl', function($scope, ActiveChallenges, ActiveChallengesFct) {
 
-  $scope.challengers = ActiveChallenges.all();
-  $scope.remove = function(challenger) {
-    ActiveChallenges.remove(challenger);
+  ActiveChallengesFct.getActiveChallenges()
+    .success(function(data) {
+      $scope.activeChallenges = data;
+    });
+  
+  $scope.removeActiveChallenge = function(activeChallenge) {
+    ActiveChallengesFct.removeActiveChallenge(activeChallenge);
   };
+
 });
