c8672c249223d87e8a1d7229f812af94eba84293
Justin Zimmerman <zimmy130@gmail.com>
2015-12-19 15:23:59 -0500

    (feat) add completed challenge logic

--- www/js/controllers/ActiveChallengesCtrl.js
+++ www/js/controllers/ActiveChallengesCtrl.js
@@ -7,6 +7,15 @@ angular.module('activechallengesctrl', [])
   ActiveChallengesFct.getActiveChallenges($scope.user.id)
     .success(function(data) {
       $scope.activeChallenges = data;
+      $scope.activeChallenges.forEach(function(challenge) {
+        if($scope.user.id !== challenge.challengeeId) {
+          challenge.opponent = challenge.challengeeName;
+        } else if($scope.user.id !== challenge.challengerId) {
+          challenge.opponent = challenge.challengerName;
+        } else {
+          challenge.opponent = "Opponent";
+        }
+      });
     });
   var userId = $scope.user.id;
   $scope.removeActiveChallenge = function(activeChallenge, userId) {
--- www/js/controllers/CompletedCtrl.js
+++ www/js/controllers/CompletedCtrl.js
@@ -7,6 +7,26 @@ angular.module('completed', [])
   CompletedFct.getCompletedChallenge($scope.user.id)
     .success(function(data) {
       $scope.challenges = data;
+      $scope.challenges.forEach(function(challenge) {
+        if($scope.user.id !== challenge.challengeeId) {
+          challenge.opponent = challenge.challengeeName;
+        } else if($scope.user.id !== challenge.challengerId) {
+          challenge.opponent = challenge.challengerName;
+        } else {
+          challenge.opponent = "Opponent";
+        }
+        if(challenge.challengeeCompleted && challenge.challengerCompleted) {
+          if($scope.user.id === challenge.winnerId) {
+            challenge.completedStatus = "You won this challenge!";
+          } else if($scope.user.id === challenge.loserId) {
+            challenge.completedStatus = "You lost this challenge.";
+          }else {
+            challenge.completedStatus = "Challenge still calculating.";
+          }
+        } else {
+          challenge.completedStatus = "Waiting for other user to complete.";
+        }
+      });
     })
     .error(function(error) {
       alert(error);
