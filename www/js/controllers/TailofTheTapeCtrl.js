78eb87d2ca1f6320cd07d9a07147b55f1bd6a75c
Anthony Mullins <anthonyjoelmullins@gmail.com>
2015-12-22 18:48:58 -0800

    (feat) Add routing for active challenge details

--- www/js/controllers/TailofTheTapeCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('tailofthetape', [])
-
-.controller('TailofTheTapeCtrl', function($scope, $stateParams, ActiveChallenges) {
-  $scope.challenger = ActiveChallenges.get($stateParams.challengerID);
-  console.log("tale of the tape ctrl");
-});
