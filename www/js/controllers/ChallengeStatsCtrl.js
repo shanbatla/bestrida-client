d413ac9edf4c3bae2cb8e44dc9ceef62f4c25a88
shanbatla <shan.batla@gmail.com>
2015-12-22 21:44:23 -0800

    (feat) Initial pseudocode.

--- www/js/controllers/ChallengeStatsCtrl.js
+++ www/js/controllers/ChallengeStatsCtrl.js
@@ -1,4 +1,50 @@
 angular.module('challengestats', [])
 
-.controller('ChallengeStatsCtrl', function($scope) {
+.controller('ChallengeStatsCtrl', function($scope, $stateParams) {
+  //Get challenge id 
+
+  //Need segment name
+  $scope.challenge; 
+  //Need distance for segment - 
+  //Who won
+  //User, and their time
+  //Opponenet, and their time
+
 });
+
+
+//Challenge object
+  //does not have time
+// { _id: '5675914fa878868b3398bd3f',
+//   segmentId: 1,
+//   segmentName: 'Tour de France',
+//   challengerId: 12348243,
+//   challengerName: 'Green Teletubbie',
+//   challengeeId: 2711071,
+//   challengeeName: 'Red Teletubbie',
+//   expires: '2015-12-25T23:27:00.000Z',
+//   __v: 0,
+//   loserName: 'Carly Fiorina',
+//   loserId: 2711071,
+//   winnerName: 'Donald Trump',
+//   winnerId: 12348243,
+//   challengeeTime: 571,
+//   challengeeAvgCadence: 15,
+//   challengeeAvgWatts: 55,
+//   challengeeAvgHeartrate: 95,
+//   challengeeMaxHeartRate: 110,
+//   challengerTime: 579,
+//   challengerAvgCadence: 10,
+//   challengerAvgWatts: 50,
+//   challengerAvgHeartrate: 100,
+//   challengerMaxHeartRate: 125,
+//   segmentCountry: 'Some Country',
+//   segmentCity: 'Some City',
+//   segmentDistance: 100,
+//   segmentElevationGain: 9,
+//   segmentAverageGrade: 5,
+//   segmentClimbCategory: 3,
+//   created: '2015-12-19T17:18:07.521Z',
+//   status: 'complete',
+//   challengeeCompleted: true,
+//   challengerCompleted: true }
