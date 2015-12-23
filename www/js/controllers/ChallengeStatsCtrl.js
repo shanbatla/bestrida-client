ddb8d7e99c1842174318f587472985a4a478aff2
shanbatla <shan.batla@gmail.com>
2015-12-23 14:30:18 -0800

    (feat) Challenge Stats page in the Completed Challenges tab (close #90).

--- www/js/controllers/ChallengeStatsCtrl.js
+++ www/js/controllers/ChallengeStatsCtrl.js
@@ -21,6 +21,9 @@ angular.module('challengestats', [])
       $scope.challenges.forEach(function(challenge) {
         if ($scope.challengeId === challenge._id) {
           
+          //Declare status
+          // $scope.challengeStatus = challenge.status;
+
           //Declare opponent
           $scope.opponent = challenge.challengeeName;
           
@@ -29,7 +32,7 @@ angular.module('challengestats', [])
           
           //Declare distance
           var distanceInMeters = challenge.segmentDistance;
-          var distanceInMiles = distanceInMeters/1609.34
+          var distanceInMiles = distanceInMeters/1609.34;
           $scope.distance = distanceInMiles;
           
           //Declare time for user and opponent, then declare winner
@@ -78,41 +81,3 @@ angular.module('challengestats', [])
     });
 
 }]);
-
-
-//Challenge object
-  //does not have time
-  //don't have distance of segment
-// { _id: '5675914fa878868b3398bd3f',
-//   segmentId: 1,
-//   segmentName: 'Tour de France',
-//   challengerId: 12348243,
-//   challengerName: 'Green Teletubbie',
-//   challengeeId: 2711071,
-//   challengeeName: 'Red Teletubbie',
-//   expires: '2015-12-25T23:27:00.000Z',
-//   __v: 0,
-//   loserName: 'Carly Fiorina',
-//   loserId: 2711071,
-//   winnerName: 'Donald Trump',
-//   winnerId: 12348243,
-//   challengeeTime: 571,
-//   challengeeAvgCadence: 15,
-//   challengeeAvgWatts: 55,
-//   challengeeAvgHeartrate: 95,
-//   challengeeMaxHeartRate: 110,
-//   challengerTime: 579,
-//   challengerAvgCadence: 10,
-//   challengerAvgWatts: 50,
-//   challengerAvgHeartrate: 100,
-//   challengerMaxHeartRate: 125,
-//   segmentCountry: 'Some Country',
-//   segmentCity: 'Some City',
-//   segmentDistance: 100,
-//   segmentElevationGain: 9,
-//   segmentAverageGrade: 5,
-//   segmentClimbCategory: 3,
-//   created: '2015-12-19T17:18:07.521Z',
-//   status: 'complete',
-//   challengeeCompleted: true,
-//   challengerCompleted: true }
