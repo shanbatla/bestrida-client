562929c7e90eabbba7f5a3880b051e988473c219
shanbatla <shan.batla@gmail.com>
2015-12-22 16:13:06 -0800

    (test) Set up client side testing (close #63).

--- test/unit.js
+++ test/unit.js
@@ -93,20 +93,20 @@ describe('Account Controller', function(){
   });
 })
 
-describe('Auth Controller', function(){
-  var scope;
+// describe('Auth Controller', function(){
+//   var scope;
 
-  // load the controller's module
-  beforeEach(module('auth', ['ngCordovaOauth']));
+//   // load the controller's module
+//   beforeEach(module('auth', ['ngCordovaOauth']));
 
-  beforeEach(inject(function($rootScope, $controller) {
-      scope = $rootScope.$new();
-      $controller('AuthCtrl', {$scope: scope});
-  }));
+//   beforeEach(inject(function($rootScope, $controller) {
+//       scope = $rootScope.$new();
+//       $controller('AuthCtrl', {$scope: scope});
+//   }));
 
-  // tests start here
-  it('should have a defined function called login', function(){
-      expect(scope.login).toBeDefined();
-  });
-})
+//   // tests start here
+//   it('should have a defined function called login', function(){
+//       expect(scope.login).toBeDefined();
+//   });
+// })
 
