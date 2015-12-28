296cbb356dd50ac49d7c21b327801ade9970b752
shanbatla <shan.batla@gmail.com>
2015-12-28 14:17:25 -0800

    (feat) Set up pending status controller; added factory to retrieve segment info.

--- www/js/controllers/PendingDetailCtrl.js
+++ www/js/controllers/PendingDetailCtrl.js
@@ -1,4 +1,31 @@
 angular.module('pendingdetail', [])
 
-.controller('PendingDetailCtrl', function($scope) {
-});
+.controller('PendingDetailCtrl', ['$scope', '$stateParams', 'ChallengeStatsFct', 'PendingDetailFct', function($scope, $stateParams, ChallengeStatsFct, PendingDetailFct) {
+  // Define:
+    // name of segment 
+    // distance
+    // city 
+    // state
+    // activity type
+    // avg grade 
+    // climb category
+
+
+  // Get challenge id, then get challenge object
+  var challengeId = $stateParams.challengeId;
+   //Ping API, get challenge
+  ChallengeStatsFct.getChallenge(challengeId)
+    .success(function(data) {
+      
+      $scope.segmentId = data.segmentId;
+
+    })
+    .error(function(data) {
+      alert('error')
+    });
+  
+
+  // Get segment and its info
+
+}]);
+
